a7975250594ad327625a8c9271b1d98d73db73f8
Oriana Contreras <ocontreras@ecomsur.com>
2022-05-07 12:53:02 -0400

    Creación de la capa de datos

---
+++ LibDataContrerasMontesinos/LibDataContrerasMontesinos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibDataContrerasMontesinos
+{
+    public class Class1
+    {
+    }
+}

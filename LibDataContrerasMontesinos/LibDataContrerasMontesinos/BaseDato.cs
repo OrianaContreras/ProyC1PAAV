ad3cd14b918257aebfe49585d426fca0031c179f
Oriana Contreras <ocontreras@ecomsur.com>
2022-05-07 13:22:32 -0400

    Creación de la clase inventario

--- LibDataContrerasMontesinos/LibDataContrerasMontesinos/BaseDato.cs
+++ LibDataContrerasMontesinos/LibDataContrerasMontesinos/BaseDato.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibNegContrerasMontesinos;
 
 namespace LibDataContrerasMontesinos
 {
@@ -14,39 +15,37 @@ namespace LibDataContrerasMontesinos
         private string strConn = ConfigurationManager.AppSettings["strConexion"];
 
         #region
-        public string ingresar(string objAmigo)
+        public Inventario ingresar(Inventario objInventario)
         {
 
             SqlCommand cmd = new SqlCommand();
             try
             {
-                //cmd.Connection = new SqlConnection("Data Source= motor_de_la_base_de_datos;Initial Catalog=nombre_de_la_base_de_datos;User Id=nombre_usuario;Password=clave_usuario");
-                //cmd.Connection = new SqlConnection("Data Source=UPR_404LAB_DCTE\\SQLSERVER2017DEV;Initial Catalog=DB_Amistades;User Id=sa;Password=Passw0rd");
                 cmd.Connection = new SqlConnection(strConn);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.CommandText = "pa_ingresar";
 
-                //cmd.Parameters.Add("@rut", SqlDbType.VarChar, 12).Value = objAmigo.Rut;
-                //cmd.Parameters.Add("@nombre", SqlDbType.VarChar, 20).Value = objAmigo.Nombre;
-                //cmd.Parameters.Add("@apPaterno", SqlDbType.VarChar, 20).Value = objAmigo.ApPaterno;
-                //cmd.Parameters.Add("@apMaterno", SqlDbType.VarChar, 20).Value = objAmigo.ApMaterno;
-                //cmd.Parameters.Add("@edad", SqlDbType.Int).Value = objAmigo.Edad;
-                //cmd.Parameters.Add("@direccion", SqlDbType.VarChar, 50).Value = objAmigo.Direccion;
-                //cmd.Parameters.Add("@fono", SqlDbType.VarChar, 20).Value = objAmigo.Fono;
-                //cmd.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = objAmigo.Email;
+                //cmd.Parameters.Add("@rut", SqlDbType.VarChar, 12).Value = objInventario.Rut;
+                //cmd.Parameters.Add("@nombre", SqlDbType.VarChar, 20).Value = objInventario.Nombre;
+                //cmd.Parameters.Add("@apPaterno", SqlDbType.VarChar, 20).Value = objInventario.ApPaterno;
+                //cmd.Parameters.Add("@apMaterno", SqlDbType.VarChar, 20).Value = objInventario.ApMaterno;
+                //cmd.Parameters.Add("@edad", SqlDbType.Int).Value = objInventario.Edad;
+                //cmd.Parameters.Add("@direccion", SqlDbType.VarChar, 50).Value = objInventario.Direccion;
+                //cmd.Parameters.Add("@fono", SqlDbType.VarChar, 20).Value = objInventario.Fono;
+                //cmd.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = objInventario.Email;
 
                 cmd.Connection.Open();
                 cmd.ExecuteNonQuery();
                 cmd.Connection.Close();
 
-                //objAmigo.EsExito = true;
+                //objInventario.EsExito = true;
             }
             catch (SqlException ex)
             {
-                //objAmigo.Mensaje = "Excepcion Capturada: {0} " + ex.ToString();
-                //objAmigo.EsExito = false;
+                //objInventario.Mensaje = "Excepcion Capturada: {0} " + ex.ToString();
+                //objInventario.EsExito = false;
             }
-            return objAmigo;
+            return objInventario;
         }// fin ingresar
 
 
---
+++ LibNegContrerasMontesinos/LibNegContrerasMontesinos/Inventario.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibDataContrerasMontesinos;
+
+namespace LibNegContrerasMontesinos
+{
+    public class Inventario
+    {
+        #region atributos
+        private string _codigo, _fecha, _seccion, _nombreArticulo, _estado, _etiquetado, _realizadoPor;
+        private string _mensaje;
+        private int _id;
+        private DataSet _ds = new DataSet();
+
+        //private bool _esExito = false;
+        #endregion
+
+        #region propiedades
+
+        public string Codigo { get => _codigo; set => _codigo = value; }
+        public string Fecha { get => _fecha; set => _fecha = value; }
+        public string Seccion { get => _seccion; set => _seccion = value; }
+        public string NombreArticulo { get => _nombreArticulo; set => _nombreArticulo = value; }
+        public string Estado { get => _estado; set => _estado = value; }
+        public string Etiquetado { get => _etiquetado; set => _etiquetado = value; }
+        public string RealizadoPor { get => _realizadoPor; set => _realizadoPor = value; }
+        public string Mensaje { get => _mensaje; set => _mensaje = value; }
+        public int Id { get => _id; set => _id = value; }
+
+        #endregion
+
+        #region operaciones
+        public Inventario ingresar(Inventario objInventario)
+        {
+            BaseDato objDB = new BaseDato();
+            objInventario = objDB.ingresar(objInventario);
+            return objInventario;
+        }// fin ingresar
+
+        //public Inventario mostrar(Inventario objInventario)
+        //{
+        //    BaseDato objDB = new BaseDato();
+        //    objInventario = objDB.mostrar(objInventario);
+        //    return objInventario;
+        //}// fin mostrar
+
+        //public Inventario eliminar(Inventario objInventario)
+        //{
+        //    BaseDato objDB = new BaseDato();
+        //    objInventario = objDB.eliminar(objInventario);
+        //    return objInventario;
+        //}// fin eliminar
+
+        //public Inventario modificar(Inventario objInventario)
+        //{
+        //    BaseDato objDB = new BaseDato();
+        //    objInventario = objDB.modificar(objInventario);
+        //    return objInventario;
+        //}// fin modificar
+
+        #endregion
+
+
+    }// Fin Class
+}// Fin namesapace
